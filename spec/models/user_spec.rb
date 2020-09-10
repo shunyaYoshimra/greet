4c5a8e389a177a5f195c37657f79280303000bc3
shunyaYoshimura <maoorgri1015@gmail.com>
2020-09-10 14:45:10 +0900

    write model tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid with username, email, password" do
+    user = User.new(
+      username: 'test-user',
+      email: 'test@example.com',
+      password: 'password'
+    )
+    expect(user).to be_valid
+  end
+
+  it "is invalid user without username" do
+    user = User.new(username: nil)
+    user.valid?
+    expect(user.errors[:username]).to include("を入力してください")
+  end
+
+  it "is invalid user without email" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("を入力してください")
+  end
+
+  it "is invalid user without password" do
+    user = User.new(password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include("を入力してください")
+  end
 end
+
